a15764e8da2db3d75c7bad45d23344ac240d7127
Jonathan Pallant <jonathan.pallant@42technology.com>
2021-05-19 17:34:00 +0100

    Work with NCS 1.5.1 (#22)
    * Work with NCS 1.5.1.
    
    We had to:
    
    * Add two heaps (a main heap and a TX only heap) which are interrupt-safe.
    * Implement the IPC peripheral driver
    * Minor changes for the new heapless crate
    
    * Build on stable, with HF
    
    * Use nrfxlib-sys 1.5.1-rc1
    
    * Fix comment mis-format.
    
    * Fix memory allocations.
    
    Now we can start the modem and bring up multiple sockets.
    
    * Bump to -sys 1.5.1 final.
    
    * Clean up allocators.
    
    They were basically the same, so now there's one function
    which is given the specific heap to use.
    
    * Cleaning up clippy lints.

--- src/tls.rs
+++ src/tls.rs
@@ -134,8 +134,7 @@ impl TlsSocket {
 
 		let mut result;
 		// Now, make a null-terminated hostname
-		let mut hostname_smallstring: heapless::String<heapless::consts::U64> =
-			heapless::String::new();
+		let mut hostname_smallstring: heapless::String<64> = heapless::String::new();
 		write!(hostname_smallstring, "{}\0", hostname).map_err(|_| Error::HostnameTooLong)?;
 		// Now call getaddrinfo with some hints
 		let hints = sys::nrf_addrinfo {
@@ -174,7 +173,7 @@ impl TlsSocket {
 					sin_len: core::mem::size_of::<sys::nrf_sockaddr_in>() as u8,
 					sin_family: sys::NRF_AF_INET as i32,
 					sin_port: htons(port),
-					sin_addr: dns_addr.sin_addr.clone(),
+					sin_addr: dns_addr.sin_addr,
 				};
 
 				debug!("Trying IP address {}", &crate::NrfSockAddrIn(connect_addr));
